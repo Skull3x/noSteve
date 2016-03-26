53cea3b806db209baf6bba5a886687234b05b515
Daniel <daniel.barinak@gmail.com>
2016-03-26 23:39:55 +0100

    Added files via upload

---
+++ src/mcunderground/noSteve/Main.php
@@ -0,0 +1,30 @@
+<?php
+/*
+written by MCUnderground
+*/
+namespace mcunderground\nosteve;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Event;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class Main extends PluginBase implements Listener {
+ 
+  public function onEnable(){
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  $this->getLogger()->info("[noSteve] Loaded!");
+  }
+ 
+  public function join(PlayerPreLoginEvent $event){ 
+$player = $event->getPlayer(); 
+$name = $player->getName();
+if($name == "Steve"){
+$player->kick(Color::RED . "User your username, not Steve",False);
+}
+}
+
+    }
